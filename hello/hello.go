6fcb5e296a74b756654d6a1c9c1ac923bcc9e013
Owen Griffiths <oweng@oweng-macbookpro.roam.corp.google.com>
2014-09-01 22:38:12 -0700

    Download raw HTML for a tag

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello %d\n", 10)
+	const filename = "/Users/oweng/Documents/tags.txt"
+	
+	lines := readFileLines(filename)
+	fmt.Printf("%d tags loaded\n", len(lines))
+	
+	checkTag(lines[0])
+}
+
+func readFileLines(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	
+	scanner := bufio.NewScanner(file)
+
+	var result []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		result = append(result, line)
+	}
+	err = scanner.Err();
+	if (err != nil) {
+		log.Fatal(err)
+	}
+	
+	return result
+}
+
+func checkTag(tag string) {
+	url := fmt.Sprintf("http://iconosquare.com/viewer.php#/tag/%s/list", tag)
+	fmt.Printf("Checking '%s'\n", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Received %d[b]\n", len(body))
 }
